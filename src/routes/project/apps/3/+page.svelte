e88a614c860bb888273f79d9c55c304a1f8c0bfb
Alex <alex@mail.com>
2023-06-09 23:31:40 +0300

    itog2

---
+++ src/routes/project/apps/3/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Модуль 1</h1>
+<main>
+  <p><a  class='link' href="https://silver-lolly-87edc3.netlify.app/">Нажми, чтобы увидеть мои первые работы</a></p>
+</main>
+
+<script>
+
+</script>
+<style>
+  .link {
+     color: rgb(10, 10, 139);
+    }
+ </style>
